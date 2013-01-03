0cd25e9899a775d32eee93004058f7e5108ea493
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-01-03 15:10:30 -0500

    Filled in a simple setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Chong',
+    version='0.1dev',
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    packages=['chong'],
+    license='LICENSE',
+    description="An implementation of the board game Chong.",
+)
