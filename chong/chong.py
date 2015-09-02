0c1d53b4b985eafc859172bbf6062f19a120e54d
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-09-02 19:47:39 -0400

    Updated board methods to match the new API.
    * play -> next_state
    * is_legal requires full state history
    * legal_plays requires full state history
    * next_player -> current_player

--- chong/chong.py
+++ chong/chong.py
@@ -110,7 +110,7 @@ class Board(object):
         r, c, s = play
         return ''.join(('P' * (1 - s), 'abcdefgh'[c], str(r)))
 
-    def play(self, state, play):
+    def next_state(self, state, play):
         r, c, s = play
         p1_xy, p2_xy, p1_placed, p2_placed, player = state
 
@@ -128,11 +128,11 @@ class Board(object):
         player = 3 - player
         return (p1_xy, p2_xy, p1_placed, p2_placed, player)
 
-    def is_legal(self, state, play):
-        plays = set(self.legal_plays(state))
+    def is_legal(self, state_history, play):
+        plays = set(self.legal_plays(state_history))
         return play in plays
 
-    def legal_plays(self, state):
+    def legal_plays(self, state_history):
         p1_xy, p2_xy, p1_placed, p2_placed, player = state
 
         if player == 1 and p1_xy == 0:
@@ -162,11 +162,11 @@ class Board(object):
 
         return jumps + pawn + placements
 
-    def next_player(self, state):
+    def current_player(self, state):
         return state[-1]
 
-    def winner(self, state_lst):
-        state = state_lst[-1]
+    def winner(self, state_history):
+        state = state_history[-1]
         p1_xy, p2_xy, p1_placed, p2_placed, player = state
 
         if p1_xy == 0 or p2_xy == 0:
@@ -175,7 +175,7 @@ class Board(object):
             return 1
         if self.inv_positions[p2_xy][0] == 0:
             return 2
-        if not self.legal_plays(state):
+        if not self.legal_plays(state_history):
             return 3 - player
         if state_lst.count(state) >= 3:
             return 3
